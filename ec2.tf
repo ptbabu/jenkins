fefe977f9066a8301e636c740fe9f46e83a6eb19
ptbabu <48373639+ptbabu@users.noreply.github.com>
2020-02-17 20:00:06 +0530

    updated

--- ec2.tf
+++ ec2.tf
@@ -1,33 +1,13 @@
-provider "aws" {
-  region = "eu-west-2"
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+provider "aws"{
+    region ="us-east-2"
+    access_key="${var.access_key}"
+    secret_key ="${var.secret_key}"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"]
-}
-
-resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
-
-  tags {
-    Name = "HelloWorld"
-  }
-}
-output "ip"{
-value= "${aws_instance.web.public_ip}"
+resource "aws_vpc" "main"{
+    cidr_block ="10.0.0.0/16"
+    instance_tenancy="default"
+    tags ={
+        Name ="tractise vpc"
+    }
 }
